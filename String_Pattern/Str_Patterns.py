c5a8d7ce51c12383cf8adaa9932fe9d0ae2113de
Ashish Rajesh Lulla <lullaashish2807@gmail.com>
2022-02-12 10:12:43 +0530

    Update Str_Patterns.py

--- String_Pattern/Str_Patterns.py
+++ String_Pattern/Str_Patterns.py
@@ -128,3 +128,108 @@ Output:
 
 P y t h o n
 """
+
+
+for row in reversed(range(len(string1))):
+    for col in range(row+1):
+        print(string1[col], end=" ")
+    print("\n")
+
+"""
+Output:
+p y t h o n
+
+p y t h o
+
+p y t h
+
+p y t
+
+p y
+
+p
+"""
+
+for row in reversed(range(len(string1))):
+    for col in range(row+1):
+        print(string1[row], end=" ")
+    print("\n")
+
+"""
+Output:
+n n n n n n
+
+o o o o o
+
+h h h h
+
+t t t
+
+y y
+
+p 
+"""
+
+for row in range(len(string1)):
+    for col in range(len(string1)-row):
+        print(string1[row], end=" ")
+    print("\n")
+
+"""
+Output:
+P P P P P P
+
+y y y y y
+
+t t t t
+
+h h h
+
+o o 
+
+n
+"""
+
+for row in range(len(string1)):
+    for col in range(row):
+        print(" ",end=" ")
+    for col in range(len(string1)-row):
+        print(string1[col], end=" ")
+    print("\n")
+
+"""
+Output:
+P y t h o n
+
+  P y t h o
+
+    P y t h
+
+      P y t
+
+        P y 
+
+          P
+"""
+
+for row in range(len(string1)):
+    for col in range(row):
+        print(" ",end=" ")
+    for col in range(len(string1)-row):
+        print(string1[row], end=" ")
+    print("\n")
+
+"""
+Output:
+P P P P P P
+
+  y y y y y
+
+    t t t t
+
+      h h h
+
+        o o
+
+          n
+"""
