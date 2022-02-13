dba27ffcb74c8b878b925fba9633ff1c8c11d66d
Ashish Rajesh Lulla <lullaashish2807@gmail.com>
2022-02-13 11:12:50 +0530

    Update int_tri.py

--- Integer_Patterns/int_tri.py
+++ Integer_Patterns/int_tri.py
@@ -128,3 +128,85 @@ Output:
 
 1 2 3 4 5
 """
+
+
+for row in range(num):
+    for col in range(num-row-1):
+        print(" ", end=" ")
+    for col in range(row, -1, -1):
+        print(col+1, end=" ")
+    print("\n")
+
+"""
+Output:
+        1
+
+      2 1
+
+    3 2 1
+
+  4 3 2 1 
+
+5 4 3 2 1
+"""
+
+for row in range(num):
+    for col in range(num-row-1):
+        print(" ", end=" ")
+    for col in range(row+1):
+        print(row+1, end=" ")
+    print("\n")
+
+"""
+Output:
+        1 
+
+      2 2
+
+    3 3 3
+
+  4 4 4 4
+
+5 5 5 5 5
+"""
+
+for row in range(num):
+    for col in range(num-row-1):
+        print(" ", end=" ")
+    for col in range(row+1):
+        print(num-row, end=" ")
+    print("\n")
+
+"""
+Output:
+        5
+
+      4 4
+
+    3 3 3
+
+  2 2 2 2
+
+1 1 1 1 1 
+"""
+
+
+for row in range(num):
+    for col in range(num-row-1):
+        print(" ", end=" ")
+    for col in range(row+1):
+        print(num-col, end=" ")
+    print("\n")
+"""
+Output:
+        5
+
+      5 4
+
+    5 4 3
+
+  5 4 3 2
+
+5 4 3 2 1
+
+"""
