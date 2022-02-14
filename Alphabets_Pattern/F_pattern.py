9f9d3b180d17baba392667a56b803150bf89f1d4
Ashish Rajesh Lulla <lullaashish2807@gmail.com>
2022-02-14 10:09:14 +0530

    Add files via upload

---
+++ Alphabets_Pattern/F_pattern.py
@@ -0,0 +1,19 @@
+for row in range(7):
+    for col in range(5):
+        if (col == 0) or (row == 0 or row == 3):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+
+"""
+Output:
+* * * * * 
+*
+*
+* * * * *
+*
+*
+*
+"""
