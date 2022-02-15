f260e40300ea3597d1885cbb4d8dc017d090eef4
Ashish Rajesh Lulla <lullaashish2807@gmail.com>
2022-02-16 01:01:06 +0530

    Add files via upload

---
+++ Alphabets_Pattern/T_pattern.py
@@ -0,0 +1,19 @@
+for row in range(7):
+    for col in range(5):
+        if (row == 0) or (col == 2):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+"""
+Output:
+
+* * * * * 
+    *
+    *
+    *
+    *
+    *
+    *
+"""
