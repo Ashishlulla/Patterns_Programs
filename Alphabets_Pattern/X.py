e17f77349e992bd0e7333cf046fc0ece9056db5c
Ashish Rajesh Lulla <lullaashish2807@gmail.com>
2022-02-19 00:37:04 +0530

    Create X.py

---
+++ Alphabets_Pattern/X.py
@@ -0,0 +1,21 @@
+i = 0
+j = 4
+for row in range(5):
+    for col in range(5):
+        if (row == i and col == j):
+            print("*", end=" ")
+            i += 1
+            j -= 1
+        elif (row == col):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+Output:
+
+*       * 
+  *   *   
+    *
+  *   *
+*       *
